4cf9699626157c36d9a23017e922130bac733051
Teddy South <tedsouth@outlook.com>
2017-04-27 23:10:57 +0100

    Playing around w events
    :3

--- main/java/fingersome/dorfcraft/ModInfo.java
+++ main/java/fingersome/dorfcraft/ModInfo.java
@@ -2,8 +2,8 @@ package fingersome.dorfcraft;
 
 public class ModInfo 
 {
-	public static final String MODID = "dorfsvsgoblins";
-    public static final String NAME = "Dwarfs vs Goblins";
+	public static final String MODID = "dorfcraft";
+    public static final String NAME = "Dorfcraft";
     public static final String VERSION = "1.0.0";
     public static final String DEPENDENCIES = "";
 }
--- main/java/fingersome/dorfcraft/event/EventManager.java
+++ main/java/fingersome/dorfcraft/event/EventManager.java
@@ -1,10 +1,17 @@
 package fingersome.dorfcraft.event;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class EventManager {
     
@@ -12,13 +19,32 @@ public class EventManager {
 	public void onItemTossEvent(ItemTossEvent event) {
 		Minecraft.getMinecraft().player.sendChatMessage("I THREW IT ON THE GROOOOOOUND~");
 	}
-	
-	
+
 	@SubscribeEvent
-	public void onPlayerInteractEvent(PlayerInteractEvent event) {
+	public void onPlayerRightClickBlock(RightClickBlock event) {
+		World world = event.getWorld();
+		BlockPos pos = event.getPos();
+		IBlockState state = world.getBlockState(pos);
+
+		if (state.getBlock() == Blocks.ANVIL) {
+			event.setUseBlock(Result.DENY);	
+			if (world.isRemote) {
+			Minecraft.getMinecraft().player.sendChatMessage("You're no Smith...");
+			}
+		}
+		else if (state.getBlock() == Blocks.ENCHANTING_TABLE) {
+			event.setUseBlock(Result.DENY);
+			if (world.isRemote) {
+			Minecraft.getMinecraft().player.sendChatMessage("You're no Enchanter...");
+			}
+		}
+		else if (state.getBlock() == Blocks.BREWING_STAND) {
+			event.setUseBlock(Result.DENY);
+			if (world.isRemote) {
+			Minecraft.getMinecraft().player.sendChatMessage("You're no Alchemist...");
+			}
+		}
 		
-		event.setCanceled(true);
-		Minecraft.getMinecraft().player.sendChatMessage("Huh... That's weird...");
 	}
 	
 	
